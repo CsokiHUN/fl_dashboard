48a8b2bee9d7430ef850ec2e40fcb974537ce402
Csoki <feketetibi71@gmail.com>
2023-02-15 14:34:53 +0100

    format(fxmanifest.lua)

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,21 +1,21 @@
-fx_version("cerulean")
-game("gta5")
-author("Csoki")
+fx_version "cerulean"
+game "gta5"
+author "Csoki"
 
-dependency("es_extended")
-dependency("oxmysql")
+dependency "es_extended"
+dependency "oxmysql"
 
-shared_scripts({ "@es_extended/imports.lua", "shared.lua" })
-client_script("client.lua")
-server_scripts({
+shared_scripts { "@es_extended/imports.lua", "shared.lua" }
+client_script "client.lua"
+server_scripts {
 	"@oxmysql/lib/MySQL.lua",
 	"premium.lua",
 	"server.lua",
-})
+}
 
-files({
+files {
 	"ui/dist/**",
-})
+}
 
-ui_page("ui/dist/index.html")
+ui_page "ui/dist/index.html"
 -- ui_page("http://192.168.1.17:8080")
